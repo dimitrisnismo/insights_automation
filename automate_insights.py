1fa15e84708d9e44c5b34ff553a048c7d85a5286
Dimitris Chortarias <dimitris.chortarias@gmail.com>
2022-07-10 00:14:55 +0300

    f1

--- automate_insights.py
+++ automate_insights.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime
+import altair as alt
 from dataset_creation import *
 from prophet import Prophet
 from prophet.diagnostics import cross_validation, performance_metrics
@@ -11,33 +12,22 @@ from prophet.plot import (
 )
 import logging
 import warnings
+import copy
 
 warnings.filterwarnings("ignore")
 logging.getLogger("fbprophet").setLevel(logging.WARNING)
 pd.options.display.float_format = "{:,.3f}".format
 
-# Creating dummy Dataset
-numberofrowsperproduct = 120000
-start = pd.to_datetime("2021-01-01")
-end = pd.to_datetime("2022-06-15")
-df = create_df(start, end, numberofrowsperproduct)
-
-
-##Checking min max dates
-print(df.groupby(["Product"]).agg({"Datetime": [np.min, np.max]}))
-print(df.groupby(["Manufacturer"]).agg({"Datetime": [np.min, np.max]}))
-
-##Groupby to day level all data
-df = df.groupby(["Product", "Manufacturer", "Datetime"]).sum().reset_index()
-
 
 def automatedInsights(df, categories, dates, value):
+    tempcategories = copy.deepcopy(categories)
     """
     Must not contain column with the name Date_val
     """
     print("Creating keys to filter the dataframe")
     dfKeys = keysCreation(df, categories)
     print(" Transform column to date type")
+    dfgrouped = dateTransform(df=df, date_column=dates)
     df = dateTransform(df=df, date_column=dates)
     print(" Creating a key for each Category")
     df = pd.merge(df, dfKeys, on=categories, how="left")
@@ -46,6 +36,8 @@ def automatedInsights(df, categories, dates, value):
     df = pd.merge(df, dfKeys, on=categories, how="left")
     print("Creating Date Utils Columns")
     df = datesUtilsColumns(df)
+    maxdate = str(df[df["Data"] == "Actual"]["Date_val"].max())
+    maxdate = str(df[df["Data"] == "Actual"]["Date_val"].max())
     print("Calculate MoM")
     momDf = mom_calculation(df, categories, value)
     print("Calculate WoW")
@@ -55,17 +47,23 @@ def automatedInsights(df, categories, dates, value):
     print("Calculate YTD")
     ytddf = ytd_calculation(df, categories, value)
     dateperiodscomparisons = create_comparisonsdf(
-        categories, momDf, wowdf, mtddf, ytddf
+        categories, momDf, wowdf, mtddf, ytddf, value
     )
-    return df, dateperiodscomparisons
+    df_compared = dateperiodscomparisons
+    print(categories)
+    vizualizeResults(modifycolumnstoviz, df_compared, maxdate, tempcategories, value)
+    highandlow(df, tempcategories, value)
+    return df, df_compared, dfgrouped
 
 
-def create_comparisonsdf(categories, momDf, wowdf, mtddf, ytddf):
+def create_comparisonsdf(categories, momDf, wowdf, mtddf, ytddf, value):
     dateperiodscomparisons = pd.concat([momDf, wowdf, mtddf, ytddf])
     categories.append("%_Difference")
     categories.append("Amount")
     categories.append("Type_period")
     categories.append("Data_max_date")
+    x = "previous_" + value
+    categories.append(x)
     dateperiodscomparisons = dateperiodscomparisons[categories]
     return dateperiodscomparisons
 
@@ -195,7 +193,7 @@ def keysCreation(data, categories):
     """
     Creating based on the given categories (columns) the keys for the dataset
     """
-    df = data.groupby(categories).count().reset_index()[categories]
+    df = data.groupby(categories).sum().reset_index()[categories]
     df["cat_number"] = df.index
     return df
 
@@ -231,10 +229,11 @@ def comparisons(categories, input_df, value, datesComparison):
             tempDf[value] / tempDf[value].shift(1) - 1,
             np.nan,
         )
+
+        tempDf["previous_" + value] = tempDf[value].shift(1)
         tempDf = tempDf[~tempDf["%_Difference"].isnull()]
         category_val = category_val + category
         tempDf["category_comparison"] = category_val
-        tempDf["previous_" + value] = tempDf[value].shift(1)
         tempDf["Data_max_date"] = input_df[input_df["Data"] == "Actual"][
             "Date_val"
         ].max()
@@ -242,6 +241,142 @@ def comparisons(categories, input_df, value, datesComparison):
     return tdf
 
 
-df, comparisons = automatedInsights(
+def modifycolumnstoviz(t2com):
+    tt2com = t2com.copy().dropna()
+    tt2com["%_Difference"] = tt2com["%_Difference"] * 100
+    tt2com["%_Difference"] = tt2com["%_Difference"].round(1)
+    tt2com["Col_label"] = np.where(
+        tt2com["%_Difference"] == tt2com["%_Difference"].max(),
+        "max",
+        np.where(
+            tt2com["%_Difference"] == tt2com["%_Difference"].min(),
+            "min",
+            "other",
+        ),
+    )
+
+    return tt2com
+
+
+def vizualizeResults(modifycolumnstoviz, df_compared, maxdate, categories, value):
+    lista = copy.deepcopy(categories)
+    lista.append("%_Difference")
+    lista.append(value)
+    lista.append("previous_" + value)
+
+    print("Below are the key metrics as of " + maxdate + " for the given dataset")
+    for periods in df_compared["Type_period"].unique():
+        tcom = df_compared[df_compared["Type_period"] == periods]
+        for category in categories[:-1]:
+            print(categories)
+            for item in tcom[category].unique():
+                print("item is :" + item)
+                print("category is :" + category)
+                t2com = tcom[tcom[category] == item]
+                t22com = t2com[t2com.isna().any(axis=1)]
+                print(
+                    """  
+        The"""
+                    + periods
+                    + "  for the "
+                    + category
+                    + ": "
+                    + item
+                    + " is "
+                    + str(round(t22com["%_Difference"].sum(), 5) * 100)
+                    + "%"
+                )
+                tt2com = modifycolumnstoviz(t2com)
+                chart = (
+                    alt.Chart(tt2com)
+                    .mark_bar()
+                    .encode(
+                        alt.X(categories[-1], scale=alt.Scale(zero=False)),
+                        alt.Y("%_Difference"),
+                        color=alt.Color(
+                            "Col_label",
+                            scale=alt.Scale(
+                                domain=["max", "min", "other"],
+                                range=[
+                                    "#00891E",
+                                    "#890000",
+                                    "#AFAFAF",
+                                ],
+                            ),
+                        ),
+                    )
+                )
+                rule = (
+                    alt.Chart(tt2com)
+                    .mark_rule(color="black")
+                    .encode(y="mean(%_Difference)")
+                )
+                (
+                    (chart + rule)
+                    .properties(title=category + " by " + periods + "  % ", width=600)
+                    .configure_title(fontSize=20, anchor="middle", color="#404040")
+                    .configure_view(strokeWidth=0)
+                    .display()
+                )
+                print(tt2com[tt2com["Col_label"] != "other"][lista])
+    return lista
+
+
+def highandlow(df, categories, value):
+    lista = []
+    for category in categories:
+        lista.append(category)
+        maxmonthdate = df["EOMDate"].max()
+        outliers = df.groupby(lista).agg({value: [min, max]}).reset_index()
+        outliers.columns = ["".join(col).strip() for col in outliers.columns.values]
+        min_outliers = outliers.rename(columns={value + "min": value})
+        minlista = copy.deepcopy(lista)
+        minlista.append(value)
+        min_outliers = pd.merge(min_outliers, df, on=minlista, how="left")
+        min_outliers["Count"] = 1
+        min_outliers = min_outliers.groupby(minlista).count().reset_index()
+        min_outliers = min_outliers[(min_outliers["Count"] == 1)]
+        min_outliers = pd.merge(min_outliers[minlista], df, on=minlista, how="left")
+        min_outliers = min_outliers[(min_outliers["EOMDate"] == maxmonthdate)]
+        if len(min_outliers) == 0:
+            print("There are no new lowest values to show for the last month")
+        else:
+            print("New  Lows!")
+            print(min_outliers)
+        max_outliers = outliers.rename(columns={value + "max": value})
+        maxlista = copy.deepcopy(lista)
+        maxlista.append(value)
+        max_outliers = pd.merge(max_outliers, df, on=maxlista, how="left")
+        max_outliers["Count"] = 1
+        max_outliers = max_outliers.groupby(maxlista).count().reset_index()
+        max_outliers = max_outliers[(max_outliers["Count"] == 1)]
+        max_outliers = pd.merge(max_outliers[maxlista], df, on=maxlista, how="left")
+        max_outliers = max_outliers[(max_outliers["EOMDate"] == maxmonthdate)]
+        print("About: " + category)
+        if len(max_outliers) == 0:
+            print("There are no new highest values to show for the last month")
+        else:
+            print("New Highs!")
+            print(max_outliers)
+
+
+# Creating dummy Dataset
+numberofrowsperproduct = 15000
+start = pd.to_datetime("2021-01-01")
+end = pd.to_datetime("2022-06-15")
+df = create_df(start, end, numberofrowsperproduct)
+
+
+##Checking min max dates
+print(df.groupby(["Product"]).agg({"Datetime": [np.min, np.max]}))
+print(df.groupby(["Manufacturer"]).agg({"Datetime": [np.min, np.max]}))
+
+
+##Groupby to day level all data
+df["Datetime"] = pd.to_datetime(df["Datetime"]).dt.date
+df = df.groupby(["Product", "Manufacturer", "Datetime"]).sum().reset_index()
+
+
+df, df_compared, dfgrouped = automatedInsights(
     df=df, categories=["Product", "Manufacturer"], dates="Datetime", value="Amount"
 )
--- dataset_creation.py
+++ dataset_creation.py
@@ -20,19 +20,32 @@ def create_df(start, end, numberofrowsperproduct):
     df_DSLR = df_temp.copy()
     df_DSLR["Product"] = "DSLR"
     df_DSLR["Manufacturer"] = np.random.choice(
-        ["Nikon", "Canon", "GoPro"], len(df_DSLR)
+        ["Nikon", "Canon", "GoPro", "Sony"], len(df_DSLR)
     )
     df_DSLR["Revenue"] = np.random.randint(350, 700, len(df_DSLR))
-    df_DSLR["Amount"] = np.where(np.random.randint(1, 10, len(df_DSLR)) <= 9, 1, 2)
+    df_DSLR["Amount"] = np.where(np.random.randint(1, 10, len(df_DSLR)) <= 2, 1, 2)
 
     # Smartphones Fake Dataset
     df_smart = df_temp.copy()
     df_smart["Product"] = "Smartphones"
     df_smart["Manufacturer"] = np.random.choice(
-        ["Apple", "Samsung", "Xiaomi"], len(df_smart)
+        [
+            "Apple",
+            "Samsung",
+            "Xiaomi",
+            "Sony",
+            "OnePlus",
+            "Huawei",
+            "TCL",
+            "Vivo",
+            "Poco",
+            "Motorola",
+            "Google",
+        ],
+        len(df_smart),
     )
     df_smart["Revenue"] = np.random.randint(200, 1500, len(df_smart))
-    df_smart["Amount"] = np.where(np.random.randint(1, 10, len(df_smart)) <= 9, 1, 2)
+    df_smart["Amount"] = np.where(np.random.randint(1, 10, len(df_smart)) <= 3, 1, 2)
     col = "Amount"
     conditions = [df_smart[col] == "Apple", df_smart[col] == "Xiaomi"]
     choices = [df_smart["Amount"] * 1.2, df_smart["Amount"] * 0.8]
@@ -41,27 +54,29 @@ def create_df(start, end, numberofrowsperproduct):
     # TVs Fake Dataset
     df_TV = df_temp.copy()
     df_TV["Product"] = "TV"
-    df_TV["Manufacturer"] = np.random.choice(["Samsung", "LG", "Sony"], len(df_TV))
+    df_TV["Manufacturer"] = np.random.choice(
+        ["Samsung", "LG", "Sony", "Xiaomi", "FU", "TOSHIBA", "PANASONIC"], len(df_TV)
+    )
     df_TV["Revenue"] = np.random.randint(700, 1900, len(df_TV))
-    df_TV["Amount"] = np.where(np.random.randint(1, 10, len(df_TV)) <= 9, 1, 2)
+    df_TV["Amount"] = np.where(np.random.randint(1, 10, len(df_TV)) <= 7, 1, 2)
 
     # Laptop Fake Dataset
     df_Laptop = df_temp.copy()
     df_Laptop["Product"] = "Laptop"
     df_Laptop["Manufacturer"] = np.random.choice(
-        ["Lenovo", "Dell", "HP"], len(df_Laptop)
+        ["Lenovo", "Dell", "HP", "Apple", "Huawei", "Acer", "Other"], len(df_Laptop)
     )
     df_Laptop["Revenue"] = np.random.randint(500, 1500, len(df_Laptop))
-    df_Laptop["Amount"] = np.where(np.random.randint(1, 10, len(df_Laptop)) <= 9, 1, 2)
+    df_Laptop["Amount"] = np.where(np.random.randint(1, 10, len(df_Laptop)) <= 6, 1, 2)
 
     # Gaming Fake Dataset
     df_Gaming = df_temp.copy()
     df_Gaming["Product"] = "Gaming"
     df_Gaming["Manufacturer"] = np.random.choice(
-        ["Sony", "Microsoft", "Nintendo"], len(df_Gaming)
+        ["Sony", "Microsoft", "Nintendo", "Steam"], len(df_Gaming)
     )
     df_Gaming["Revenue"] = np.random.randint(250, 600, len(df_Gaming))
-    df_Gaming["Amount"] = np.where(np.random.randint(1, 10, len(df_Gaming)) <= 9, 1, 2)
+    df_Gaming["Amount"] = np.where(np.random.randint(1, 10, len(df_Gaming)) <= 4, 1, 2)
 
     df = pd.concat([df_DSLR, df_smart, df_Gaming, df_TV, df_Laptop])
     return df
